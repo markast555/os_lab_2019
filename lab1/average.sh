fd4a48c16471af91f8dc9dc9749ff79c1edd9f75
markast555 <mark.ast555@gmail.com>
2024-09-24 20:40:33 +0000

    добавлен average.sh, numbers.txt.

--- lab1/average.sh
+++ lab1/average.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 
 if test "$#" -eq 0
 then
@@ -7,3 +7,22 @@ then
 fi
 
 echo "Количество введённых аргументов: $#"
+
+# Инициализация переменной для суммы
+sum=0
+
+# Цикл для суммирования всех аргументов
+for arg in "$@"
+do
+    # Проверка, является ли аргумент числом с помощью expr
+    if ! expr "$arg" + 0 &> /dev/null
+    then
+        echo "Ошибка: '$arg' не является числом."
+        exit 1
+    fi
+    sum=$(echo "$sum + $arg" | bc)
+done
+
+average=$(echo "$sum / $#" | bc -l)
+
+echo "Среднее арифметическое: $average"
